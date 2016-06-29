1603f4d672d6351e4a1d0de739f538c85cc14a33
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-06-29 16:45:40 -0300

    Update zsh

--- scripts/zsh.mk
+++ scripts/zsh.mk
@@ -1,6 +1,11 @@
-zsh-install: install-zsh
+zsh-install: install-zsh install-terminator install-ohmyz
 
 install-zsh:
-	sudo apt-get install zsh terminator -y
+	sudo apt-get install zsh -y
+
+install-terminator:
+	sudo apt-get install terminator -y
+
+install-ohmyz:
 	wget –no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh
 	chsh -s /bin/zsh
