a9b57cb630d5e9923f5c17f844ffd19792978d67
Felipe Norato Lacerda <fnoratol@gmail.com>
2017-05-23 10:24:52 -0300

    Update sublime.mk

--- scripts/sublime.mk
+++ scripts/sublime.mk
@@ -1,5 +1,6 @@
 sublime: install-sublime
 
 install-sublime:
-	wget -O sublime https://download.sublimetext.com/sublime-text_build-3114_amd64.deb
-	sudo dpkg -i sublime && rm sublime
+	sudo add-apt-repository ppa:webupd8team/sublime-text-3
+	sudo apt-get update
+	sudo apt-get install sublime-text-installer
